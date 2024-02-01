cc37bf95d6f2442534edfc908b3b985301e5f56d
mrdvelvin@gmail.com <mrdvelvin@gmail.com>
2024-02-01 15:29:39 +0400

    Update companies.php

--- routes/companies.php
+++ routes/companies.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['auth:user', 'lang'])->group(function () {
-
     //Company Routes
     Route::get('/companies', [CompanyController::class, 'index']);
     Route::post('/companies', [CompanyController::class, 'store']);
