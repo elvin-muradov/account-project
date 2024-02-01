89fff6e28a9de309e03695d86d71fa0d0540f8d9
mrdvelvin@gmail.com <mrdvelvin@gmail.com>
2024-02-01 14:56:00 +0400

    aaa

--- app/Http/Controllers/Api/V1/EnumsController.php
+++ app/Http/Controllers/Api/V1/EnumsController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
 
 class EnumsController extends Controller
 {
+    use HttpResponses;
     public function companyCategories(): array
     {
         return [
