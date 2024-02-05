476bdb3028c8771d9e0593b549db5ff421aea25e
mrdvelvin@gmail.com <mrdvelvin@gmail.com>
2024-02-05 14:10:54 +0400

    Type fixed

--- app/Http/Controllers/Api/V1/Companies/MainDocumentController.php
+++ app/Http/Controllers/Api/V1/Companies/MainDocumentController.php
@@ -23,10 +23,6 @@ class MainDocumentController extends Controller
 
         $type = $request->input('type');
 
-        $companyFiles = [
-            'tax_id_number_files'
-        ];
-
         if ($company) {
             return match ($type) {
                 CompanyMainDocuments::tax_id_number_files->value => $this->success(data: $company->tax_id_number_files),
