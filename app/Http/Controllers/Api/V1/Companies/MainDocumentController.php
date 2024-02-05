7e5ac86516fdacc108835e8ba1c4f94fecddce2c
mrdvelvin@gmail.com <mrdvelvin@gmail.com>
2024-02-05 15:47:41 +0400

    Update MainDocumentController.php

--- app/Http/Controllers/Api/V1/Companies/MainDocumentController.php
+++ app/Http/Controllers/Api/V1/Companies/MainDocumentController.php
@@ -23,6 +23,8 @@ class MainDocumentController extends Controller
 
         $type = $request->input('type');
 
+        dd($request->all());
+
         if ($company) {
             return match ($type) {
                 CompanyMainDocuments::tax_id_number_files->value => $this->success(data: $company->tax_id_number_files),
