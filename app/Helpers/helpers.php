f2f9461553ce0b0cc713b57712148ba6f067fb76
mrdvelvin@gmail.com <mrdvelvin@gmail.com>
2024-02-08 23:28:01 +0400

    Update helpers.php

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -39,7 +39,7 @@ if (!function_exists("returnFilesArray")) {
             ));
 
             $returnedArray[] = [
-                'path' => env('BASE_URL') . '/api/' . $strForPath . '/' . $fileName,
+                'path' => env('BASE_URL') . '/' . $strForPath . '/' . $fileName,
                 'bucket' => $strForPath,
                 'generated_name' => $fileName,
                 'original_name' => $file->getClientOriginalName(),
@@ -72,7 +72,7 @@ if (!function_exists("returnOrderFile")) {
         ));
 
         $returnedArray[] = [
-            'path' => env('BASE_URL') . '/api/' . $bucket . '/' . $fileName,
+            'path' => env('BASE_URL') . '/' . $bucket . '/' . $fileName,
             'bucket' => $bucket,
             'generated_name' => $fileName,
             'original_name' => $fileName,
