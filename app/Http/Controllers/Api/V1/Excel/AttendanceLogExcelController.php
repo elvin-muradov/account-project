9585d715727789852eb1be4d4b580d6173b79ed0
mrdvelvin@gmail.com <mrdvelvin@gmail.com>
2024-10-16 17:41:24 +0400

    Update AttendanceLogExcelController.php

--- app/Http/Controllers/Api/V1/Excel/AttendanceLogExcelController.php
+++ app/Http/Controllers/Api/V1/Excel/AttendanceLogExcelController.php
@@ -12,13 +12,11 @@ class AttendanceLogExcelController extends Controller
 {
     public function exportAttendanceLogExcel(Request $request): BinaryFileResponse
     {
-        $request->validate([
+        $req = $request->validate([
             'company_id' => ['required', 'integer', 'exists:companies,id'],
             'month' => ['required', 'integer', 'between:1,12'],
             'year' => ['required', 'integer', 'between:1900,' . date('Y')]
         ]);
-
-        $req = $request->only('company_id', 'month', 'year');
 
         return Excel::download(new AttendanceLogExport($req), 'attendance_log_export.xlsx');
     }
