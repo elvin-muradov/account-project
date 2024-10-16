465f213d6515a53ac3e61d765a8bf2cc86128896
mrdvelvin@gmail.com <mrdvelvin@gmail.com>
2024-10-16 17:56:46 +0400

    Update attendance_log_export.blade.php

--- resources/views/exports/attendance_logs/attendance_log_export.blade.php
+++ resources/views/exports/attendance_logs/attendance_log_export.blade.php
@@ -11,128 +11,128 @@
 <table style="border:2px solid black">
     <thead>
     <tr>
-        <th class=@php echo $cellTDClass; @endphp rowspan="3">№</th>
-        <th class=@php echo $cellTDClass; @endphp rowspan="3">Soyadı, Adı</th>
-        <th class=@php echo $cellTDClass; @endphp rowspan="3">Vəzifəsi</th>
-        <th class=@php echo $cellTDClass; @endphp colspan="31" rowspan="2">AYIN&nbsp;&nbsp;&nbsp;GÜNLƏRİ</th>
-        <th class=@php echo $cellTDClass; @endphp rowspan="3">Ayda işlədiyi günləri</th>
-        <th class=@php echo $cellTDClass; @endphp rowspan="3"> İstirahət və bayram&nbsp;&nbsp;&nbsp;günləri</th>
-        <th class=@php echo $cellTDClass; @endphp rowspan="3">İşlənmiş saatlar</th>
+        <th @php echo $cellTDClass; @endphp rowspan="3">№</th>
+        <th @php echo $cellTDClass; @endphp rowspan="3">Soyadı, Adı</th>
+        <th @php echo $cellTDClass; @endphp rowspan="3">Vəzifəsi</th>
+        <th @php echo $cellTDClass; @endphp colspan="31" rowspan="2">AYIN&nbsp;&nbsp;&nbsp;GÜNLƏRİ</th>
+        <th @php echo $cellTDClass; @endphp rowspan="3">Ayda işlədiyi günləri</th>
+        <th @php echo $cellTDClass; @endphp rowspan="3"> İstirahət və bayram&nbsp;&nbsp;&nbsp;günləri</th>
+        <th @php echo $cellTDClass; @endphp rowspan="3">İşlənmiş saatlar</th>
     </tr>
     <tr>
     </tr>
     <tr>
-        <th class=@php echo $cellTDClass; @endphp>1</th>
-        <th class=@php echo $cellTDClass; @endphp>2</th>
-        <th class=@php echo $cellTDClass; @endphp>3</th>
-        <th class=@php echo $cellTDClass; @endphp>4</th>
-        <th class=@php echo $cellTDClass; @endphp>5</th>
-        <th class=@php echo $cellTDClass; @endphp>6</th>
-        <th class=@php echo $cellTDClass; @endphp>7</th>
-        <th class=@php echo $cellTDClass; @endphp>8</th>
-        <th class=@php echo $cellTDClass; @endphp>9</th>
-        <th class=@php echo $cellTDClass; @endphp>10</th>
-        <th class=@php echo $cellTDClass; @endphp>11</th>
-        <th class=@php echo $cellTDClass; @endphp>12</th>
-        <th class=@php echo $cellTDClass; @endphp>13</th>
-        <th class=@php echo $cellTDClass; @endphp>14</th>
-        <th class=@php echo $cellTDClass; @endphp>15</th>
-        <th class=@php echo $cellTDClass; @endphp>16</th>
-        <th class=@php echo $cellTDClass; @endphp>17</th>
-        <th class=@php echo $cellTDClass; @endphp>18</th>
-        <th class=@php echo $cellTDClass; @endphp>19</th>
-        <th class=@php echo $cellTDClass; @endphp>20</th>
-        <th class=@php echo $cellTDClass; @endphp>21</th>
-        <th class=@php echo $cellTDClass; @endphp>22</th>
-        <th class=@php echo $cellTDClass; @endphp>23</th>
-        <th class=@php echo $cellTDClass; @endphp>24</th>
-        <th class=@php echo $cellTDClass; @endphp>25</th>
-        <th class=@php echo $cellTDClass; @endphp>26</th>
-        <th class=@php echo $cellTDClass; @endphp>27</th>
-        <th class=@php echo $cellTDClass; @endphp>28</th>
-        <th class=@php echo $cellTDClass; @endphp>29</th>
-        <th class=@php echo $cellTDClass; @endphp>30</th>
-        <th class=@php echo $cellTDClass; @endphp>31</th>
+        <th @php echo $cellTDClass; @endphp>1</th>
+        <th @php echo $cellTDClass; @endphp>2</th>
+        <th @php echo $cellTDClass; @endphp>3</th>
+        <th @php echo $cellTDClass; @endphp>4</th>
+        <th @php echo $cellTDClass; @endphp>5</th>
+        <th @php echo $cellTDClass; @endphp>6</th>
+        <th @php echo $cellTDClass; @endphp>7</th>
+        <th @php echo $cellTDClass; @endphp>8</th>
+        <th @php echo $cellTDClass; @endphp>9</th>
+        <th @php echo $cellTDClass; @endphp>10</th>
+        <th @php echo $cellTDClass; @endphp>11</th>
+        <th @php echo $cellTDClass; @endphp>12</th>
+        <th @php echo $cellTDClass; @endphp>13</th>
+        <th @php echo $cellTDClass; @endphp>14</th>
+        <th @php echo $cellTDClass; @endphp>15</th>
+        <th @php echo $cellTDClass; @endphp>16</th>
+        <th @php echo $cellTDClass; @endphp>17</th>
+        <th @php echo $cellTDClass; @endphp>18</th>
+        <th @php echo $cellTDClass; @endphp>19</th>
+        <th @php echo $cellTDClass; @endphp>20</th>
+        <th @php echo $cellTDClass; @endphp>21</th>
+        <th @php echo $cellTDClass; @endphp>22</th>
+        <th @php echo $cellTDClass; @endphp>23</th>
+        <th @php echo $cellTDClass; @endphp>24</th>
+        <th @php echo $cellTDClass; @endphp>25</th>
+        <th @php echo $cellTDClass; @endphp>26</th>
+        <th @php echo $cellTDClass; @endphp>27</th>
+        <th @php echo $cellTDClass; @endphp>28</th>
+        <th @php echo $cellTDClass; @endphp>29</th>
+        <th @php echo $cellTDClass; @endphp>30</th>
+        <th @php echo $cellTDClass; @endphp>31</th>
     </tr>
     </thead>
     <tbody>
     <tr>
-        <td class=@php echo $cellTDClass; @endphp>1</td>
-        <td class=@php echo $cellTDClass; @endphp>XXXXXXXXX</td>
-        <td class=@php echo $cellTDClass; @endphp>XXXXXX</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>7</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>B</td>
-        <td class=@php echo $cellTDClass; @endphp>B</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>19</td>
-        <td class=@php echo $cellTDClass; @endphp>12</td>
-        <td class=@php echo $cellTDClass; @endphp>151</td>
+        <td @php echo $cellTDClass; @endphp>1</td>
+        <td @php echo $cellTDClass; @endphp>XXXXXXXXX</td>
+        <td @php echo $cellTDClass; @endphp>XXXXXX</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>7</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>B</td>
+        <td @php echo $cellTDClass; @endphp>B</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>19</td>
+        <td @php echo $cellTDClass; @endphp>12</td>
+        <td @php echo $cellTDClass; @endphp>151</td>
     </tr>
     <tr>
-        <td class=@php echo $cellTDClass; @endphp>2</td>
-        <td class=@php echo $cellTDClass; @endphp>XXXXXXXXX</td>
-        <td class=@php echo $cellTDClass; @endphp>Direktor</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>7</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>B</td>
-        <td class=@php echo $cellTDClass; @endphp>B</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>8</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>i</td>
-        <td class=@php echo $cellTDClass; @endphp>19</td>
-        <td class=@php echo $cellTDClass; @endphp>12</td>
-        <td class=@php echo $cellTDClass; @endphp>151</td>
+        <td @php echo $cellTDClass; @endphp>2</td>
+        <td @php echo $cellTDClass; @endphp>XXXXXXXXX</td>
+        <td @php echo $cellTDClass; @endphp>Direktor</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>7</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>B</td>
+        <td @php echo $cellTDClass; @endphp>B</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>8</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>i</td>
+        <td @php echo $cellTDClass; @endphp>19</td>
+        <td @php echo $cellTDClass; @endphp>12</td>
+        <td @php echo $cellTDClass; @endphp>151</td>
     </tr>
     </tbody>
 </table>
