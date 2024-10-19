d8584d2054cec811d52b1fc0aee69c020192d5e7
mrdvelvin@gmail.com <mrdvelvin@gmail.com>
2024-10-20 00:04:42 +0400

    Update salary_calculate_export.blade.php

--- resources/views/exports/salary_calculate/salary_calculate_export.blade.php
+++ resources/views/exports/salary_calculate/salary_calculate_export.blade.php
@@ -68,9 +68,9 @@
             <th @php echo $cellHeadClass200Width; @endphp>Soyadı, Adı</th>
             <th @php echo $cellHeadClass200Width; @endphp>Vəzifəsi</th>
             <th @php echo $cellHeadClass100Width; @endphp>Maaş</th>
-            <th @php echo $cellHeadClass200Width; @endphp>Ayda iş <br/>saatları</th>
-            <th @php echo $cellHeadClass200Width; @endphp>Faktiki <br/>iş saatları</th>
-            <th @php echo $cellHeadClass200Width; @endphp>Hesablanmış <br/>əmək haqqı</th>
+            <th @php echo $cellHeadClass100Width; @endphp>Ayda iş <br/>saatları</th>
+            <th @php echo $cellHeadClass100Width; @endphp>Faktiki <br/>iş saatları</th>
+            <th @php echo $cellHeadClass100Width; @endphp>Hesablanmış <br/>əmək haqqı</th>
             <th @php echo $cellHeadClass100Width; @endphp>Mükafat</th>
             <th @php echo $cellHeadClass200Width; @endphp>Məzuniyyət</th>
             <th @php echo $cellHeadClass100Width; @endphp>CƏMİ</th>
@@ -78,8 +78,8 @@
             <th @php echo $cellHeadClass200Width; @endphp>Pensiya Fondu (10%)</th>
             <th @php echo $cellHeadClass100Width; @endphp>İ.S.H (0.5%)</th>
             <th @php echo $cellHeadClass100Width; @endphp>İ.T.S.H (2%)</th>
-            <th @php echo $cellHeadClass200Width; @endphp>Cəmi <br/>Tutulmuşdur</th>
-            <th @php echo $cellHeadClass200Width; @endphp>Ödəniləsi <br/>Məbləğ</th>
+            <th @php echo $cellHeadClass100Width; @endphp>Cəmi <br/>Tutulmuşdur</th>
+            <th @php echo $cellHeadClass100Width; @endphp>Ödəniləsi <br/>Məbləğ</th>
         </tr>
         </thead>
         <tbody>
