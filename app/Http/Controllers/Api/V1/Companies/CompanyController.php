ad6fa5899433bcee080f554da711f668be800a12
mrdvelvin@gmail.com <mrdvelvin@gmail.com>
2024-10-25 15:45:14 +0400

    Update CompanyController.php

--- app/Http/Controllers/Api/V1/Companies/CompanyController.php
+++ app/Http/Controllers/Api/V1/Companies/CompanyController.php
@@ -24,6 +24,8 @@ class CompanyController extends Controller
         $authUser = auth()->user()->load('companiesServed');
         $companiesServed = $authUser->companiesServed()->pluck('id')->toArray();
 
+        dd($authUser->hasRole(['leading_expert', 'department_head']));
+
         $companies = Company::search($request->input('filter_by'), $request->input('logic_operator'))
             ->order($request->input('order_by'))
             ->with(['mainUser', 'director'])
