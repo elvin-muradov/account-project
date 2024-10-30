cd438977571b9190ca9c3610c16c99c8da141405
mrdvelvin@gmail.com <mrdvelvin@gmail.com>
2024-10-30 16:17:29 +0400

    Update ValidateCompanyID.php

--- app/Http/Middleware/ValidateCompanyID.php
+++ app/Http/Middleware/ValidateCompanyID.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Middleware;
 
+use App\Traits\HttpResponses;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ValidateCompanyID
 {
+    use HttpResponses;
     /**
      * Handle an incoming request.
      *
