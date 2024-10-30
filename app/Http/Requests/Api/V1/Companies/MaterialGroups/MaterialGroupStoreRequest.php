790ad02bf7e0bf4240dcca53e3977e11f63086f6
mrdvelvin@gmail.com <mrdvelvin@gmail.com>
2024-10-30 16:34:56 +0400

    Update MaterialGroupStoreRequest.php

--- app/Http/Requests/Api/V1/Companies/MaterialGroups/MaterialGroupStoreRequest.php
+++ app/Http/Requests/Api/V1/Companies/MaterialGroups/MaterialGroupStoreRequest.php
@@ -26,6 +26,7 @@ class MaterialGroupStoreRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255', Rule::unique('material_groups', 'name')
                 ->where('company_id', $this->header('company-id'))],
+            'company-id' => ['required', 'integer', 'exists:companies,id'],
         ];
     }
 }
